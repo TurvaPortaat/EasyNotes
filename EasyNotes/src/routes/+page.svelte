9abc53b7f4b88c2985966c921989b650a49cd637
KirsiL <117641781+TurvaPortaat@users.noreply.github.com>
2023-12-30 23:49:47 +0200

    First commit
    Lets go

---
+++ EasyNotes/src/routes/+page.svelte
@@ -0,0 +1,50 @@
+<script>
+</script>
+
+<main>
+
+<h1>Heippa Juho!</h1>
+<p>Tässä palautan vain "jotain"</p>
+<p>Ja toivon että saan uusintamahdollisuuden</p>
+<p>Koska aika loppui kesken</p>
+<p>Oon oikeesti ihan ok näissä hommissa</p>
+<p>Kunhan vaan saan tehdä </p> 
+<h2>ajan kanssa omaan tahtii</h2>
+<p>Enkä vaa yritä tuskissani koota jotain</p>
+
+</main>
+
+<style>
+    :global(body) {
+        background-color: palegoldenrod;
+    }
+    :global(div){
+        background-color:darkolivegreen;
+    }
+    h1 {
+        color: white;
+        background-color: black;
+        font-family: Helvetica;
+        display: grid;
+        justify-content: center;
+        justify-items: center;
+        margin: 0.5em;
+        border-radius: 0.5em;
+    }
+
+    p {
+        color: burlywood;
+        font-family: 'Lucida Sans Unicode';
+        font-weight: bold;
+        display: grid;
+        justify-content: center;
+        justify-items: center;
+
+    }
+    h2 {
+        color: burlywood;
+        display: grid;
+    justify-content: center;
+    justify-items: center;
+    }
+</style>
